5600b5bf34cf8ec7f97690a4f41ba8eac2aa6fa3
Michał <michalnadolny192@gmail.com>
2021-11-13 23:23:05 +0100

    Added basic project structure

---
+++ pizza-website/src/main/java/com/teammichu/pizzawebsite/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.teammichu.pizzawebsite.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+}
