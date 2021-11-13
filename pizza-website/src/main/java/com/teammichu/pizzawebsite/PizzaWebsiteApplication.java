431d4a68c670ac18762272ed7ea35ba169862bba
Michał <michalnadolny192@gmail.com>
2021-11-13 22:44:38 +0100

    Added basic project structure

---
+++ pizza-website/src/main/java/com/teammichu/pizzawebsite/PizzaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.teammichu.pizzawebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaWebsiteApplication.class, args);
+	}
+
+}
---
+++ pizza-website/src/test/java/com/teammichu/pizzawebsite/PizzaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teammichu.pizzawebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
